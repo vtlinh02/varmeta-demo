c2f6e305a23ba51720080920723613eb611c186a
Le Huu Hoan <hoanle396@gmail.com>
2024-07-22 13:31:40 +0700

    update project apis

--- apps/server/src/services/projects.service.ts
+++ apps/server/src/services/projects.service.ts
@@ -1,64 +1,33 @@
 import { connection } from '@/databases/connection'
-import { Categories } from '@/databases/entities/Categories'
-import { Features } from '@/databases/entities/Features'
-import { Glossaries } from '@/databases/entities/Glossaries'
-import { Partnerships } from '@/databases/entities/Partnerships'
-import { ProjectDescriptions } from '@/databases/entities/ProjectDescriptions'
 import { Projects } from '@/databases/entities/Projects'
-import { ProjectSocials } from '@/databases/entities/ProjectSocials'
 import { ProjectTags } from '@/databases/entities/ProjectTags'
-import { Socials } from '@/databases/entities/Socials'
-import { Tags } from '@/databases/entities/Tags'
 import { FilterProjectOption } from '@/decorator/types'
 import { getMeta } from '@/shared/get-meta'
 import { orderParser } from '@/shared/parser-pagination'
 import { logger } from '@/utils/logger'
-import { ILike, Repository } from 'typeorm'
+import { Repository } from 'typeorm'
 
 export class ProjectsService {
   constructor(
-    private projectRepository: Repository<Projects> = connection.getRepository(
-      Projects,
-    ),
-    private socialRepository: Repository<Socials> = connection.getRepository(
-      Socials,
-    ),
+    private projectRepository: Repository<Projects> = connection.getRepository(Projects),
+    private projectTagsRepository: Repository<ProjectTags> = connection.getRepository(ProjectTags),
   ) {}
 
   public async getProjects(params?: FilterProjectOption) {
-    const {
-      page = 1,
-      page_size = 10,
-      order,
-      search,
-      category_id,
-      sub_category_id,
-      tag_id,
-    } = params
+    const { page = 1, page_size = 10, order, search, category_id, sub_category_id, tag_id } = params
     try {
       const query = await this.projectRepository
         .createQueryBuilder('project')
-        .leftJoin(
-          'project.category',
-          'sub_category',
-          'sub_category.id = project.category_id',
-        )
-        .leftJoin(Categories, 'category', 'sub_category.parent_id=category.id')
-        .leftJoin(
-          'project.projectTags',
-          'project_tags',
-          'project_tags.project_id=project.id',
-        )
-        .leftJoinAndMapMany(
-          'project.tags',
-          Tags,
-          'tags',
-          'project_tags.tag_id=tags.id',
-        )
+        .leftJoinAndSelect('project.category', 'category')
+        .leftJoinAndSelect('category.parent', 'parent_category')
+        .leftJoinAndSelect('project.projectTags', 'project_tags')
+        .leftJoinAndSelect('project_tags.tag', 'tags')
         .skip((page - 1) * page_size)
         .take(page_size)
       if (search) {
-        query.where({ name: ILike(`%${search}%`) })
+        query.andWhere('lower(project.name) LIKE lower(:search)', {
+          search: `%${search}%`,
+        })
       }
       if (category_id) {
         query.andWhere('category.id = :category_id', { category_id })
@@ -94,108 +63,51 @@ export class ProjectsService {
     try {
       const project = await this.projectRepository
         .createQueryBuilder('project')
-        .leftJoinAndMapOne(
-          'project.description',
-          ProjectDescriptions,
-          'description',
-          'project.id=description.id',
-        )
-        .leftJoin(
-          'project.projectTags',
-          'project_tags',
-          'project_tags.project_id=project.id',
-        )
-        .leftJoinAndMapMany(
-          'project.tags',
-          Tags,
-          'tags',
-          'project_tags.tag_id=tags.id',
-        )
-        .leftJoinAndMapOne(
-          'project.sub_category',
-          Categories,
-          'sub_category',
-          'sub_category.id = project.category_id',
-        )
-        .leftJoinAndMapOne(
-          'project.category',
-          Categories,
-          'category',
-          'category.id = sub_category.parent_id',
-        )
-        .leftJoinAndMapMany(
-          'project.partnerships',
-          Partnerships,
-          'partnerships',
-          'partnerships.project_id = project.id',
-        )
-        .leftJoin('project.glossaryProjects', 'glossary_projects')
-        .leftJoinAndMapMany(
-          'project.glossaries',
-          Glossaries,
-          'glossaries',
-          'glossary_projects.glossary_id=glossaries.id',
-        )
+        .leftJoinAndSelect('project.category', 'category')
+        .leftJoinAndSelect('category.parent', 'parent_category')
+        .leftJoinAndSelect('project.projectTags', 'project_tags')
+        .leftJoinAndSelect('project_tags.tag', 'tags')
+        .leftJoinAndSelect('project.projectDescriptions', 'description')
+        .leftJoinAndSelect('project.partnerships', 'partnerships')
+        .leftJoinAndSelect('project.glossaryProjects', 'glossary_projects')
+        .leftJoinAndSelect('glossary_projects.glossary', 'glossary')
+        .leftJoinAndSelect('project.projectSocials', 'project_socials')
+        .leftJoinAndSelect('project_socials.social', 'social')
         .where('project.id=:id', { id: +id })
         .getOne()
 
-      const socials = await this.socialRepository
-        .createQueryBuilder('socials')
-        .leftJoin(
-          ProjectSocials,
-          'project_socials',
-          'project_socials.social_id=socials.id',
-        )
-        .select([
-          'socials.name AS name',
-          'socials.code AS code',
-          'socials.icon_url AS iconUrl',
-          'project_socials.id as id',
-          'project_socials.url AS url',
-        ])
-        .where('project_socials.project_id = :project_id', {
-          project_id: id,
-        })
-        .getRawMany()
+      const compares = await this.getCompareByProject(id)
 
-      ;(project as Projects & { socials: Array<Socials> }).socials = socials
-
-      const compare = await Promise.all(
-        ((project as Projects & { tags: Array<Tags> }).tags ?? []).map(
-          async (tag) => {
-            const query = await this.projectRepository
-              .createQueryBuilder('project')
-              .leftJoin(
-                'project.projectTags',
-                'project_tags',
-                'project_tags.project_id=project.id',
-              )
-              .leftJoin(
-                'project.projectFeatures',
-                'project_features',
-                'project_features.project_id=project.id',
-              )
-              .leftJoinAndMapMany(
-                'project.features',
-                Features,
-                'features',
-                'project_features.feature_id=features.id',
-              )
-              .take(5)
-              .where('project_tags.tag_id = :tag_id', { tag_id: tag.id })
-              .getMany()
-            return {
-              tag,
-              data: query,
-            }
-          },
-        ),
-      )
-
-      ;(project as any).compare = compare
-      return project
+      return { ...project, compares }
     } catch (error) {
       logger.error(error)
+      throw new Error(error?.message ?? 'Something went wrong')
+    }
+  }
+
+  public async getCompareByProject(project_id: string) {
+    try {
+      const tags = await this.projectTagsRepository
+        .createQueryBuilder('project_tags')
+        .leftJoinAndSelect('project_tags.tag', 'tag')
+        .where('project_tags.project_id = :project_id', { project_id })
+        .getMany()
+
+      const result = await Promise.all(
+        (tags ?? []).map(async (tag) => {
+          const data = await this.projectRepository
+            .createQueryBuilder('project')
+            .leftJoin('project.projectTags', 'project_tags')
+            .leftJoinAndSelect('project.projectFeatures', 'project_features')
+            .leftJoinAndSelect('project_features.feature', 'feature')
+            .andWhere('project_tags.tag_id = :tag', { tag: tag.tagId })
+            .take(5)
+            .getMany()
+          return { tag: tag.tag, data }
+        }),
+      )
+      return result
+    } catch (error) {
       throw new Error(error?.message ?? 'Something went wrong')
     }
   }
