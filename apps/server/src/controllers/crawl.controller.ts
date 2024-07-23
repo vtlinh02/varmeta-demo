838c5859fe18ca176dd4e146a4afa48dc4e7b93f
Vũ Tuấn Linh <86487806+vtlinh02@users.noreply.github.com>
2024-07-23 13:33:40 +0700

    feat: add bitcoin project
    commit

---
+++ apps/server/src/controllers/crawl.controller.ts
@@ -0,0 +1,23 @@
+import { CrawlService } from '@/services/crawl.service'
+import { Request, Response } from 'express'
+
+export class CrawlController {
+  constructor(private crawlService = new CrawlService()) {}
+
+  public crawl = async (req: Request, res: Response) => {
+    // @ts-ignore
+    const fileChangesRaw: string = req.query.dataRaw
+
+    const fileChanges = this.handleFileChangeRaw(fileChangesRaw)
+
+    console.log(fileChanges)
+    if (fileChanges.length !== 0) this.crawlService.crawl(fileChanges)
+
+    res.send('successful')
+  }
+
+  private handleFileChangeRaw(data: string): Array<string> {
+    const index = data.indexOf('[')
+    return JSON.parse(data.substring(index))
+  }
+}
