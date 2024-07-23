d5998d48d45bdb1da2e3a637157f89d80e4f225f
Vũ Tuấn Linh <86487806+vtlinh02@users.noreply.github.com>
2024-07-23 13:33:40 +0700

    feat: add bitcoin project
    commit
    
    commit

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -5,12 +5,14 @@ import CategoriesRoute from './routes/categories.route'
 import ProjectsRoute from './routes/projects.route'
 import TagsRoute from './routes/tags.route'
 import { BaseRoute } from './routes/base.route'
+import { CrawlRoute } from './routes/crawl.route'
 
 const app = new App([
   new CategoriesRoute(),
   new ProjectsRoute(),
   new TagsRoute(),
   new BaseRoute(),
+  new CrawlRoute(),
 ])
 
 app.listen()
